cdb0db3a4c820094603a7e84c7ef1d87bd3e903e
Ihar Hrachyshka <ihar.hrachyshka@gmail.com>
2026-05-27 20:11:10 -0400

    pve-qemu: use upstream QEMU release tarball
    The previous fixed-output fetch cloned pve-qemu with submodules and then
    ran Meson subproject downloads in postFetch. That made the source hash
    depend on download behavior outside the pinned Proxmox commit, which can
    drift underneath CI.
    
    Use the official QEMU release tarball as the build source instead. It
    already includes the release packagecache for Meson subprojects, while
    the Proxmox git checkout is kept separately for patches, helper scripts,
    and update metadata.
    
    The update script now updates the Proxmox patch source first and then
    refreshes the upstream QEMU tarball hash for the same package version.
    
    Note: the patch also cleans up more issues in the package, e.g.
    redundant build dependencies or wrong phases used.

--- pkgs/pve-qemu/default.nix
+++ pkgs/pve-qemu/default.nix
@@ -1,97 +1,93 @@
 {
+  common-updater-scripts,
   lib,
+  nix,
   qemu,
+  fetchurl,
   fetchgit,
   proxmox-backup-qemu,
   perl540,
-  pkg-config,
-  meson,
-  cacert,
-  git,
-  pve-update-script,
+  pve-update,
+  writeShellScript,
 }:
 
 let
+  pveSrc = fetchgit {
+    url = "git://git.proxmox.com/git/pve-qemu.git";
+    rev = "a7c7a6b2b1aa75360d914b252dfcb05506ce590b";
+    hash = "sha256-b3rzpOF7PBK3uZBksPLHbkxR8/GQ8drwWEbo7hmYniM=";
+    fetchSubmodules = false;
+  };
+
   perlDeps = with perl540.pkgs; [ JSON ];
   perlEnv = perl540.withPackages (_: perlDeps);
 in
-(qemu.overrideAttrs (old: rec {
-  pname = "pve-qemu";
-  version = "10.1.2-7";
+(qemu.overrideAttrs (
+  finalAttrs: old:
+  let
+    qemuVersion = lib.head (lib.splitString "-" finalAttrs.version);
+  in
+  {
+    pname = "pve-qemu";
+    version = "10.1.2-7";
 
-  src =
-    (fetchgit {
-      url = "git://git.proxmox.com/git/pve-qemu.git";
-      rev = "a7c7a6b2b1aa75360d914b252dfcb05506ce590b";
-      hash = "sha256-TE7FgMTzzlbuUgP/04UC0FzazNRfpFEbHfSUVXrT08U=";
-      fetchSubmodules = true;
+    src = fetchurl {
+      url = "https://download.qemu.org/qemu-${qemuVersion}.tar.xz";
+      hash = "sha256-nXXzMcGly5tuuP2fZPVj7C6rNGyCLLl/izXNgtPxFHk=";
+    };
 
-      # Download subprojects managed by meson
-      postFetch = ''
-        cd "$out/qemu"
-        export NIX_SSL_CERT_FILE=${cacert}/etc/ssl/certs/ca-bundle.crt
-        export PATH="${git}/bin:$PATH"
-        for prj in subprojects/*.wrap; do
-          ${lib.getExe meson} subprojects download "$(basename "$prj" .wrap)"
-        done
-        find subprojects -type d -name .git -prune -execdir rm -r {} +
-        rm -rf subprojects/packagecache/tmp*
+    patches =
+      let
+        series = builtins.readFile "${pveSrc}/debian/patches/series";
+        patchList = builtins.filter (patch: patch != "") (lib.splitString "\n" series);
+        patchPathsList = map (patch: "${pveSrc}/debian/patches/${patch}") patchList;
+      in
+      old.patches ++ patchPathsList;
+
+    sourceRoot = "qemu-${qemuVersion}";
+
+    buildInputs = old.buildInputs ++ [ proxmox-backup-qemu ];
+
+    postPatch =
+      old.postPatch
+      + ''
+        cp ${proxmox-backup-qemu}/lib/proxmox-backup-qemu.h .
       '';
-    }).overrideAttrs
-      (_: {
-        GIT_CONFIG_COUNT = 2;
 
-        GIT_CONFIG_KEY_0 = "url.https://github.com/qemu/u-boot-sam460ex.git.insteadOf";
-        GIT_CONFIG_VALUE_0 = "https://gitlab.com/qemu-project/u-boot-sam460ex.git";
+    # Generate cpu flag files and machine versions json
+    # This is done in /debian/rules of pve-qemu, and needed by pve-qemu-server
+    postInstall = old.postInstall + ''
+      $out/bin/qemu-system-x86_64 -cpu help \
+        | ${perlEnv}/bin/perl ${pveSrc}/debian/parse-cpu-flags.pl > $out/share/qemu/recognized-CPUID-flags-x86_64
+      $out/bin/qemu-system-x86_64 -machine help \
+        | ${perlEnv}/bin/perl ${pveSrc}/debian/parse-machines.pl > $out/share/qemu/machine-versions-x86_64.json
+    '';
 
-        GIT_CONFIG_KEY_1 = "url.https://github.com/u-boot/u-boot.git.insteadOf";
-        GIT_CONFIG_VALUE_1 = "https://gitlab.com/qemu-project/u-boot.git";
-      });
+    passthru = (old.passthru or { }) // {
+      inherit pveSrc;
 
-  patches =
-    let
-      series = builtins.readFile "${src}/debian/patches/series";
-      patchList = builtins.filter (patch: builtins.isString patch && patch != "") (
-        builtins.split "\n" series
-      );
-      patchPathsList = map (patch: "${src}/debian/patches/${patch}") patchList;
-    in
-    old.patches ++ patchPathsList;
+      updateScript = writeShellScript "update-pve-qemu" ''
+        set -euo pipefail
 
-  sourceRoot = "${src.name}/qemu";
+        attr="''${1:-''${UPDATE_NIX_ATTR_PATH:-pve-qemu}}"
 
-  buildInputs = old.buildInputs ++ [ proxmox-backup-qemu ];
-  propagatedBuildInputs = [ proxmox-backup-qemu ];
+        ${lib.getExe pve-update} \
+          --deb-name pve-qemu-kvm \
+          --source-key pveSrc \
+          "$attr"
 
-  preBuild = ''
-    cp ${proxmox-backup-qemu}/lib/proxmox-backup-qemu.h .
-  ''
-  + old.preBuild;
+        version="$(${nix}/bin/nix eval --raw ".#$attr.version")"
+        ${common-updater-scripts}/bin/update-source-version \
+          "$attr" \
+          "$version" \
+          --source-key=src \
+          --ignore-same-version
+      '';
+    };
 
-  nativeBuildInputs = old.nativeBuildInputs ++ [
-    proxmox-backup-qemu
-    perlEnv
-    pkg-config
-  ];
-
-  # Generate cpu flag files and machine versions json
-  # This is done in /debian/rules of pve-qemu, and needed by pve-qemu-server
-  postInstall = old.postInstall + ''
-    $out/bin/qemu-system-x86_64 -cpu help \
-      | ${perlEnv}/bin/perl ${src}/debian/parse-cpu-flags.pl > $out/share/qemu/recognized-CPUID-flags-x86_64
-    $out/bin/qemu-system-x86_64 -machine help \
-      | ${perlEnv}/bin/perl ${src}/debian/parse-machines.pl > $out/share/qemu/machine-versions-x86_64.json
-  '';
-
-  passthru.updateScript = pve-update-script {
-    extraArgs = [
-      "--deb-name"
-      "pve-qemu-kvm"
-    ];
-  };
-
-  meta.position = builtins.dirOf ./.;
-})).override
+    meta.position = dirOf ./.;
+  }
+)).override
   {
     glusterfsSupport = true;
     enableDocs = false;
